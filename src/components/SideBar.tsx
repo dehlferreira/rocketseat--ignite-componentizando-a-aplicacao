6ff4e03e15a97b30e4dbb8ed208eccfb32d5d3fe
André Luiz Ferreira <andre.ferreira@labsit.io>
2021-06-15 00:39:13 -0300

    solução do desafio

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface SideBarProps {
+  setSelectedGenreId: (id: number) => void;
+  selectedGenreId: number;
+  genres: GenreDTO[];
+}
+
+interface GenreDTO {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar({ setSelectedGenreId, selectedGenreId, genres }: SideBarProps) {
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
